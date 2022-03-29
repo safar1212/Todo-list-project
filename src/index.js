d35030539660d0a5ab1483e2a6fe1a213a75742d
safar1212 <mm9240584@gmail.com>
2022-03-29 20:04:08 +0500

    Complete the tasks, and fix the linters errors

--- src/index.js
+++ src/index.js
@@ -1,22 +1,46 @@
+/* eslint-disable no-unused-vars */
+
 import _ from 'lodash';
 
 import './style.css';
 
-
 const listOfObjects = [
-    {
-      description: 'finish project',
-      completed: false,
-      index: 0,
-    },
-    {
-      description: 'go for a walk',
-      completed: false,
-      index: 1,
-    },
-    {
-      description: 'cook dinner',
-      completed: false,
-      index: 2,
-    },
-  ];
+  {
+    description: 'Complete tasks',
+    completed: false,
+    index: 0,
+  },
+  {
+    description: 'Check for linters',
+    completed: false,
+    index: 1,
+  },
+  {
+    description: 'Take a rest',
+    completed: false,
+    index: 2,
+  },
+];
+
+const listDiv = document.querySelector('#list');
+
+window.addEventListener('load', () => {
+  for (let i = 0; i < listOfObjects.length; i += 1) {
+    const listItem = document.createElement('div');
+    listItem.setAttribute('class', 'items');
+    const checkbox = document.createElement('input');
+    checkbox.type = 'checkbox';
+    checkbox.name = 'name';
+    checkbox.id = 'id';
+    checkbox.value = 'value';
+
+    const label = document.createElement('label');
+    label.htmlFor = 'id';
+    label.appendChild(document.createTextNode(listOfObjects[i].description));
+
+    listItem.appendChild(checkbox);
+    listItem.appendChild(label);
+
+    listDiv.appendChild(listItem);
+  }
+});
